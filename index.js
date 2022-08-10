adcb3306d144c76767fbe8485c9da5489a813e1f
nishattasnim08 <nishattasnim24258@gmail.com>
2022-08-11 00:17:01 +0600

    jwt implementation part-1

--- index.js
+++ index.js
@@ -14,16 +14,49 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const eventCollections = client.db('EventCollection').collection('events');
+        const userCollections = client.db('userCollection').collection('users');
 
-       app.get('/events', async(req, res) => {
-        const result = await eventCollections.find().toArray();
-        res.send(result)
-       })
+        app.get('/events', async (req, res) => {
+            const result = await eventCollections.find().toArray();
+            res.send(result)
+        })
 
-        app.post('/events', async(req, res) => {
+
+        // for jwt 
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollections.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const query = {
+                userName: users.userName,
+                userEmail: users.email
+            }
+            const results = await userCollections.insertOne(query);
+            res.send(results);
+        })
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollections.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+        // for jwt 
+
+
+        app.post('/events', async (req, res) => {
             const events = req.body;
             const query = {
                 eventName: events.eventName,
@@ -35,31 +68,31 @@ async function run(){
             res.send(results);
         })
 
-        app.delete('/event/:id', async(req, res) => {
+        app.delete('/event/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await eventCollections.deleteOne(query);
             res.send(result);
         })
 
-        app.put('/event/:id', async(req, res) => {
-           const id= req.params.id;
-           const data = req.body;
-           const query = {_id: ObjectId(id)};
-           const options = { upsert: true };
-           const updateDoc = {
-            $set: {
-                eventName: data.eventName,
-                eventType: data.event,
-                description: data.description,
-                dateTime: data.dateTime
-            },
-          };
-          const result = await eventCollections.updateOne(query, updateDoc, options);
-          res.send(result);
+        app.put('/event/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    eventName: data.eventName,
+                    eventType: data.event,
+                    description: data.description,
+                    dateTime: data.dateTime
+                },
+            };
+            const result = await eventCollections.updateOne(query, updateDoc, options);
+            res.send(result);
         })
 
-    }finally{
+    } finally {
 
     }
 }
@@ -71,5 +104,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-   console.log('Listening the port', port)
+    console.log('Listening the port', port)
 })
