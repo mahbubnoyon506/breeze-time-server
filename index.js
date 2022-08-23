7a5e7681f42188dc25e46f3f20fd5c12c3daad69
Noman01 <neamulhaque2002@gmail.com>
2022-08-23 11:54:02 +0600

    add nodemailer system with some error

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shcob.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
