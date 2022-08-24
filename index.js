5738d679fe9cfe934021ce51efddcd69fe5b921c
nishattasnim08 <nishattasnim24258@gmail.com>
2022-08-25 00:43:59 +0600

    Customer Review and About section added

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
         const professionalCollection = client.db('professionalCollection').collection('professional')
         const notificationCollections = client.db('notificationCollection').collection('eventNotifications');
         const packagesCollections = client.db('packagesCollection').collection('packages');
+        const reviewCollections = client.db('reviewCollection').collection('reviews');
 
 
 
@@ -171,6 +172,23 @@ async function run() {
             res.send({ result, token });
         })
         // for jwt 
+
+
+
+        // review section
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollections.find().toArray()
+            res.send(result);
+        })
+
+        app.post('/reviews', async (req, res) => {
+            const query = req.body;
+            const results = await reviewCollections.insertOne(query);
+            res.send(results);
+        })
+        // review section
+
+
 
         app.get('/events', async (req, res) => {
             const result = await eventCollections.find().toArray();
